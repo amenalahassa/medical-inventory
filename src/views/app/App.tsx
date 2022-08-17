32380e65d631b74767fa7b2b9fe744b1bcf76290
ALAHASSA Konrad <am.konrad21@gmail.com>
2022-08-17 22:36:34 +0100

    Structure project folders

---
+++ src/views/app/App.tsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+import React from "react";
+import Header from "../../components/header/Header";
+
+function App() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
+
+export default App;
