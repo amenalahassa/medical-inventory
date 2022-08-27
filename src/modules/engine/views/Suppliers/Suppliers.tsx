3cc968679c972672ba381c1437b03ea01c8c3ec3
ALAHASSA Konrad <am.konrad21@gmail.com>
2022-08-27 02:07:37 +0100

    Add supplier page header

--- src/modules/engine/views/Suppliers/Suppliers.tsx
+++ src/modules/engine/views/Suppliers/Suppliers.tsx
@@ -1,8 +1,24 @@
 import "./Suppliers.scss";
 import React from "react";
+import { Button, PageHeader } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 
 function Suppliers() {
-  return <div>Hello Suppliers</div>;
+  return (
+    <section className="supplier-page-root">
+      <PageHeader
+        className="section-content-header"
+        title="Fournisseurs"
+        subTitle="Ajouter vos contacts fournisseurs"
+        extra={[
+          <Button key="1" type="primary" className="add-supplier-btn">
+            <PlusOutlined />
+            Ajouter un fournisseur
+          </Button>,
+        ]}
+      />
+    </section>
+  );
 }
 
 export default Suppliers;
