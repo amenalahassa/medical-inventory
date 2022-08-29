f12b5680763490867d120ec84b1e0b7b111f13ff
ALAHASSA Konrad <am.konrad21@gmail.com>
2022-08-29 16:49:00 +0100

    Add supplier page empty content

--- src/modules/engine/views/Suppliers/Suppliers.tsx
+++ src/modules/engine/views/Suppliers/Suppliers.tsx
@@ -1,7 +1,8 @@
 import "./Suppliers.scss";
 import React from "react";
-import { Button, PageHeader } from "antd";
+import { Button, Empty, PageHeader } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
+import { EmptyBoxLogo } from "../../../../assets/images/svg/export";
 
 function Suppliers() {
   return (
@@ -17,6 +18,27 @@ function Suppliers() {
           </Button>,
         ]}
       />
+      <div className="supplier-page-content">
+        <Empty
+          image={<EmptyBoxLogo />}
+          imageStyle={{
+            height: 150,
+          }}
+          description={
+            <div>
+              <span>Votre liste de fournisseur est vide</span>
+              <p>
+                Un fournisseur est une personne ou une entreprise qui vous fournit
+                vos marchandises.
+              </p>
+            </div>
+          }
+        >
+          <Button type="primary" className="add-supplier-btn">
+            Ajouter un fournisseur
+          </Button>
+        </Empty>
+      </div>
     </section>
   );
 }
